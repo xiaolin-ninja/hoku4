a6daefedc067eec71c12715cf3b612a23996b567
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-04-13 11:33:48 -0700

    finished, untested

--- server.py
+++ server.py
@@ -1,50 +1,42 @@
 from flask import Flask
-from dateutil import parser
-import json
+from helpers import parse_webhooks, convert_UTC
+
 # ------------------------------------------------- #
 
 app = Flask(__name__)
-app.secret_key = 'supertopsecret'
 
 @app.route("/runs", methods=['POST'])
-def time_states():
+def time_states(webhooks):
 	"""Takes in state changes data of a test run 
 		and returns time spent on each state.
-		>>> time_states()
+		>>> time_states('test-runs.json')
 		{ "pending_seconds": 3,
 		  "creating_seconds": 7,
 		  "building_seconds": 10,
 		  "running_seconds": 2
 		}
 	"""
-	
-1. collect all statuses
-2. measure time of status
-
-for x in list:
-	x['status']
-	pending
-	creating 
-	building 
-	running 
-
-	** ignore 
-	errored
-	failed
-	succeeded
-	cancelled
-	x['created_at']
-	x['updated_at']
-
-def load_webhooks(json_file):
-	for w in json_file:
-
-
-def load_webhooks_data(json_file):
-	with open(json_file) as f:
-		webhooks = json.loads(f.read())
-		return webhooks
+	# the parser returns a list of tuples where:
+	# tuple[0] = status
+	# tuple[1] = start time
+	# tuple[2] = end time
+	data = parse_webhooks(webhooks)
+	state_times = {}
+	for d in data:
+		status = d[0]
+		time = (convert_UTC(d[2]) - convert_UTC(d[1])).total_seconds()
+		if status == 'pending':
+			state_times['pending_seconds'] += time
+		elif status == 'creating':
+			state_times['creating_seconds'] += time
+		elif status == 'building':
+			state_times['building_seconds'] += time
+		elif status == 'running':
+			state_times['running_seconds'] += time
+		else:
+			return 'error, status not found'
+	return status_times
 
 # ------------------------------------------------- #
 
-app.run(port=8080)
+app.listen(port=8080)
--- testing.py
+++ testing.py
@@ -4,17 +4,32 @@
 # ds2 = "2018-02-08T19:54:31Z"
 # d1 = parser.parse(ds1)
 # d2 = parser.parse(ds2)
+
 # secs = (d2-d1).total_seconds()
 # print(secs)
 import json
 
-def load_webhooks_data(json_file):
+def parse_webhooks(json_file):
+	"""parses testing data to extract testing states, start times, & end times
+	   omits any data from a terminal state ('errored', 'failed', 'suceeded', or 'cancelled')
+	   collects results in a list of tuples, where
+	   tuple[0] = status
+	   tuple[1] = start time (string)
+	   tuple[2] = end time (string) 
+	"""
+
 	with open(json_file) as f:
 		webhooks = json.loads(f.read())
+		# list of terminal states we want ot ignore
+		terminal = ['errored', 'failed', 'succeeded', 'cancelled']
+		results = []
 		for w in webhooks:
-			print w.keys()
-			# print('status', w['status'])
-			# print('start', w['created_at'])
-			# print('end', w['updated_at'])
+		# the state information is nested under 'data'
+			data = w['data']
+			status = data['status']
+			if status not in terminal:
+				results.append((status, data['created_at'], data['updated_at']))
 
-load_webhooks_data('test-runs.json')
+		return results
+
+print(parse_webhooks('test-runs.json'))
